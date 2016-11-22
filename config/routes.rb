6a53ebab5f58a57a715a1c730b5ae3796fe59dec
Jake Fishbain <jakefishbain@gmail.com>
2016-11-22 09:51:56 -0600

    remove first customization trial

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :contests 
   resources :squares
 
   root to: 'contests#index'
 end
-
-
-# Some setup you must do manually if you haven't yet:
-
-#   Ensure you have overridden routes for generated controllers in your routes.rb.
-#   For example:
-
-#     Rails.application.routes.draw do
-#       devise_for :users, controllers: {
-#         sessions: 'users/sessions'
-#       }
-#     end
--- db/migrate/20161122151315_add_lastname_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddLastnameToUsers < ActiveRecord::Migration[5.0]
-  def change
-    add_column :users, :lastname, :string
-  end
-end
