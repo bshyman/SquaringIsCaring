898d68f98a8f0feb7e46b7ae98336d5bdcdbce91
Jake Fishbain <jakefishbain@gmail.com>
2016-11-23 16:11:04 -0600

    begin cells migration

---
+++ db/migrate/20161123220650_create_cells.rb
@@ -0,0 +1,12 @@
+class CreateCells < ActiveRecord::Migration[5.0]
+  def change
+    create_table :cells do |t|
+      t.integer :contest_id, null:false
+      t.integer :user_id, null:false
+      t.string :position, array: true, default: []
+      t.string :result, array: true, default: []
+
+      t.timestamps(null: false)
+    end
+  end
+end
