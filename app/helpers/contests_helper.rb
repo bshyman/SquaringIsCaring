d1c9d91e79ab4242d3d574197b5f3e0227bf2b69
Benjamin Shyman <bshyman@gmail.com>
2016-11-24 14:04:40 -0600

    change id of cells to reflect row

--- app/helpers/contests_helper.rb
+++ app/helpers/contests_helper.rb
@@ -12,18 +12,18 @@ module ContestsHelper
 	end
 
 	def away_axis_populater(board, x)
-		axis_val = board.away_axis[x]
+		@@axis_val = board.away_axis[x]
 		return "<div class='row #{x}'>
-	  <div class='col s1 away_num'>" +  axis_val + "</div>"
+	  <div class='col s1 away_num'>" +  @@axis_val + "</div>"
 	end
 
-	def row_checkbox_populater
+	def row_checkbox_populater(row)
 		z = 0
 		string = ''
 		10.times do 
 			string +=
 			"<div class='col s1 square'>
-			  <input type='checkbox' id='#{z}' name='cell[position][]' value='#{z}'>
+			  <input type='checkbox' id='#{row.to_s}#{z.to_s}' name='cell[position][]' value='#{z}'>
 			  <label for='#{z}'></label>
 			</div>"
 			z += 1
