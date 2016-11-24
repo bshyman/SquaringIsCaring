52ce90a8ce53851a00a239fe13e66552ae224c46
Benjamin Shyman <bshyman@gmail.com>
2016-11-24 13:43:30 -0600

    auto populating board

--- app/helpers/contests_helper.rb
+++ app/helpers/contests_helper.rb
@@ -1,37 +1,4 @@
 module ContestsHelper
-
-	# def checkerboard(board)
-	# 	i = 0
-	# 	x = 1
-	# 	board_string = ""
-	# 10.times do 
-	# 	board_string += "<div class='row"
-	# 	board_string += x.to_s 
-	# 	board_string += "'>"
-	# 	p board_string
-	# 	board_string += "<div class='col s1 away_num'>"
-	# 	 board_string += board.away_axis[0] 
-	# 	 p "after rubyyyyyyy"
-	# 	 p board_string
-	# 	 x += 1
-	# 	10.times do 
-	# 		board_string += "<div class='col s1 square'>"
-	# 		board_string +=
- #     "<input type='checkbox' id="
- #     board_string += i.to_s
-
- #     board_string += "name='cell' value="
-	#  board_string += i.to_s
-	#  board_string += "/><label for="
-	#  board_string += i.to_s
-	#  board_string += "></label></div></div>"
-	# 			i += 1
-	# 		end
-	# 		board_string += "<br>"
-	# 	end
-	# 	board_string
-	# end
-
 	def home_axis_populater(board)
 		i = 0
 		home_axis_string = ""
@@ -44,9 +11,10 @@ module ContestsHelper
   		home_axis_string 
 	end
 
-	def away_axis_populater(board)
-		return "<div class='row one'>
-	  <div class='col s1 away_num'>" + board.away_axis[0] + "</div>"
+	def away_axis_populater(board, x)
+		axis_val = board.away_axis[x]
+		return "<div class='row #{x}'>
+	  <div class='col s1 away_num'>" +  axis_val + "</div>"
 	end
 
 	def row_checkbox_populater
@@ -60,7 +28,10 @@ module ContestsHelper
 			</div>"
 			z += 1
 		end
-		return string
+		return string + "</div>"
 	end
 	
+	def ten_rows
+
+	end
 end
