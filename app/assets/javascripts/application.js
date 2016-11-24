cdbbacca67c1c10a6608b590eb6e204cb237a3e1
Benjamin Shyman <bshyman@gmail.com>
2016-11-23 23:06:56 -0600

    debugging

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,12 +14,3 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-
-$( document ).ready(function(){
-  $('select').material_select();
-
-  $('.datepicker').pickadate({
-    selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15 // Creates a dropdown of 15 years to control year
-  });
-})
--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,6 +1,9 @@
 $( document ).ready(function(){
-	$(".dropdown-button").dropdown({
-	  inDuration: 500,
+	
+$( document ).ready(function(){
+  $('select').material_select();
+  $(".dropdown-button").dropdown({
+    inDuration: 500,
       outDuration: 500,
       constrain_width: false, // Does not change width of dropdown to that of the activator
       hover: true, // Activate on hover
@@ -8,4 +11,10 @@ $( document ).ready(function(){
       belowOrigin: false, // Displays dropdown below the button
       alignment: 'left' // Displays dropdown with edge aligned to the left of button
     });
+
+  $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15 // Creates a dropdown of 15 years to control year
+  });
+})
   })
