017dd9ae05684c8519ff25dacc832931bd904921
Jake Fishbain <jakefishbain@gmail.com>
2016-12-02 16:16:03 -0600

    DO NOT MERGE - google user login/signup

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,6 +1,7 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def all
+    #use to test what is being returned
     # raise request.env["omniauth.auth"].to_yaml
     user = User.from_omniauth(request.env["omniauth.auth"])
     if user.persisted?
