22732a4cca629598dd84a4d49686b93402629661
Benjamin Shyman <bshyman@gmail.com>
2016-12-20 13:44:19 -0500

    Restrict access for admins

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,14 +1,14 @@
 module UsersHelper
 
-	# def admin?
-	# 	current_user.admin? 
-	# end
+	def admin?
+		current_user.admin? 
+	end
 
 	# def current_user
 	# 	@cached_user = @cached_user || User.find_by(id: session[:user_id])
 	# end
 
-	# def logged_in?
- #    	current_user != nil
- #  	end
+	def logged_in?
+    	current_user != nil
+  	end
 end
