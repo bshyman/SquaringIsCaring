20d214679a0b4c091639a04db960d1742774fe98
Benjamin Shyman <bshyman@gmail.com>
2016-12-19 21:33:15 -0500

    unstash js

--- app/assets/javascripts/board.js
+++ app/assets/javascripts/board.js
@@ -1,7 +1,27 @@
 $( document ).ready(function(){
-  $(".square").on('click', function(event){
+  $("div.container").on('click', "div.square", function(event){
     alert($(this).html());
+    $(this).css('background', 'red');
 
-    $(this).css('background', 'red')
+    var cellId = $(this).children().attr("id");
+
+    var contestId = $(this).parent().parent().attr("action").slice(-7);
+    console.log("conID - " + contestId)
+    // console.log("cellid - " + cellId)
+
   })
-})
+  // $.ajax({
+  // 	method:'POST',
+  // 	url: "/contests/" +  
+  // })
+
+
+
+//     $.ajax({
+//       url: "/songs/" + songId,
+//       method: "DELETE"
+//     }).done(function(response){
+//       $row.remove();
+//     });
+//   })
+});
