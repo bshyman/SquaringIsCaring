33b445e24c16fc1917f994c9e7ee28bea4ac02cd
Benjamin Shyman <bshyman@gmail.com>
2016-12-21 12:16:26 -0500

    Add all custom form params to devise strong params

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,10 +3,10 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :avatar, :birthday, :street_address, :apt_num,  :city, :state, :zip, :phone_num)
   end
 
   def account_update_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :avatar, :birthday, :street_address, :apt_num,  :city, :state, :zip, :phone_num, :current_password)
   end
 end
