1edbb8ff76839fac20849bebd7ac91758731cc0a
Benjamin Shyman <bshyman@gmail.com>
2016-12-21 12:03:01 -0500

    add first and last name to strong params in devise source code

--- app/controllers/cells_controller.rb
+++ app/controllers/cells_controller.rb
@@ -1,22 +1,29 @@
 class CellsController < ApplicationController
 
 	def create
+		p current_user
+
 		@board = Contest.find_by(id: params[:contest_id])
 		@cell = Cell.new(cell_params)
-
-		if request.xhr?
+		p "PARRRRRRAAAAMMMMMS"
+		p params
+		p @cell.inspect
+		p current_user
+		# if request.xhr?
 			if @cell.save
+				p "SAVVEEEEDDD"
 				remove_selected_num(@board, number)			
 			else
-				flash[:notice] = "Error. Cell not saved"
+				flash[:error] = "Error. Cell not saved"
 				# redirect_to new_user_registration_path
+				p "NOT SAVED"
 			end
 
-		else
-			flash[:notice] = "Error. No ajax"
-		end
+		# else
+		# 	flash[:notice] = "Error. No ajax"
+		# end
 
-		render '_board'
+		redirect_to contest_path(@board)
 	end
 
 	private
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :admins
   devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks"} 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, :controllers => { registrations: 'registrations' }
+
   
   resources :contests do
   	resources :cells, except: [:index, :show]
