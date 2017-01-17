bdea9ead6c7038faeb2ac8de66c256697369e0d7
Benjamin Shyman <bshyman@gmail.com>
2017-01-17 17:29:30 -0600

    Fix checkbox issue

--- app/assets/javascripts/board.js
+++ app/assets/javascripts/board.js
@@ -1,45 +1,50 @@
-$( document ).ready(function(){
-	var numPool = document.getElementById("num_pool");
-	var contestId = $("form#form").attr("action").charAt(10);
+  $( document ).ready(function(){
+  	var numPool = document.getElementById("num_pool");
 
-    $.get
-    ("/contests/" + contestId, function(result){
-    	console.log(result)
-
-       var availNums = result.nums
-       for (i=0; i < availNums.length; i++) {
-       	// console.log("Here")
-        
-        // function disableCheckbox(id) {
-  var element = document.getElementById(availNums[i]);
- 
-    $(element).parent().parent().css("background-color", "white");
-    $(element).attr("disabled", true);
-    // $(element).children().css("opacity", 0);
-
-    // console.log(element)
-  // elem.style.color = "red";
-// }
-        // console.log(availNums[i])
-       }
-
-
-
-    $("div.container").on('click', "div.square", function(event){
-	// console.log(JSON.stringify(numPool))
-
-	// alert($(this).html());
-	// event.preventDefault();
-  var test = $(this).find("input[disabled='disabled']").attr("id")
-  console.log(test)
-  // if (this.children().children().attr("disabled").val("disabled")) {};
-	$(this).css('background', 'blue');
-
-	var cellId = $(this).children().attr("id");
-	var cellRoute = "/contests/" + contestId + "/cells";
-
-	});
+    var contestId = $("div.container").on('click', "div.contest-card", function(){
+      var id = $("div.contest-card").attr("id")
+      $.get("/contests/" + id, function(result){
+        var availNums = result.nums
+        for (i=0; i < availNums.length; i++) {
+          if (availNums[i]) {};
+          var element = document.getElementById(availNums[i]);
+        };
+      });
+      
     });
+    
+      $("div.container").on("click", "div.square", function(){
+        var contestId = $("form#form").attr("action").match(/\d+/);
+         alert($(this).html());
+        $(this).css("background-color", "red");
+        $("input[type=checkbox]", this).attr("checked", "checked");
+        // alert("Contest ID is " + contestId + " and this is " + $(this).html())
+      })
+
+
+
+
+
+    // $("div.container").on("click", "a#view_board", function(){
+    //   populateNums();
+    // });
+
+
+  // $("div.container").on('click', "div.square", function(event){
+  //  var contestId = $("form#form").attr("action").charAt(10);
+  //       	// console.log(JSON.stringify(numPool))
+
+  //       	alert($(this).html());
+  //       	// event.preventDefault();
+  //         var test = $(this).find("input[disabled='disabled']").attr("id")
+  //         console.log("Test - " + this)
+  //         // if (this.children().children().attr("disabled").val("disabled")) {};
+  //         $(this).css('background', 'blue');
+
+  //         var cellId = $(this).children().attr("id");
+  //         var cellRoute = "/contests/" + contestId + "/cells";
+  //       });
+
 });
 
 
--- app/controllers/cells_controller.rb
+++ app/controllers/cells_controller.rb
@@ -4,26 +4,19 @@ class CellsController < ApplicationController
 		@board = Contest.find_by(id: params[:contest_id])
 		@cell = Cell.new(cell_params)
 		@cell.user_id = current_user.id
-
-		# p "PARRRRRRAAAAMMMMMS"
-		# p params
-		# p @cell.inspect
-		# if request.xhr?
-			if @cell.save
-				new_board = remove_selected_nums(@board, @cell.position)
-				# @cell.save
-				@board.update_attributes(:available_nums => new_board)
-			else
-				flash[:error] = "Error. Cell not saved"
-				# redirect_to new_user_registration_path
-				p "NOT SAVED"
-			end
+		@cell.save
+		new_board = remove_selected_nums(@board, @cell.position)
+		if @cell.persisted?
+			@board.update_attributes(:available_nums => new_board)
+		else
+			flash[:error] = "Error. Cell not saved"
+			redirect_to login_path
+		end
 		redirect_to contest_path(@board)
 	end
 
-	private
+private
 	def cell_params
-		params.require(:cell).permit(:user_id, :contest_id, :position => [])
+		params.require(:cell).permit(:user_id, :contest_id, :cell, :position => [])
 	end
-
 end
