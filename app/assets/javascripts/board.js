d63bb555c90512804101dbd8507e12a2313fbc1e
Benjamin Shyman <bshyman@gmail.com>
2017-01-20 12:56:15 -0600

    color changes for edge cases

--- app/assets/javascripts/board.js
+++ app/assets/javascripts/board.js
@@ -1,73 +1,53 @@
   $( document ).ready(function(){
   	var numPool = document.getElementById("num_pool");
     var contestId = $("form#form").attr("action").charAt(10);
-    $("div.container").on('click', "button#display_board", function(e){
-        var numString = $("div#num_pool").text();
-        var numString = numString.replace(/\n/g, "");
-        var numString = numString.match(/\d{1,2}/g)
-        console.log(numString)
-        for (i = 0; i < 100; i++) {
-              // debugger
-          if (!numString.includes(i.toString())) {
-              // console.log(i + " - " + numString[i])
-            console.log(i)
-            var taken = document.getElementById(i.toString())
-            $(taken).parent().parent().addClass( "black");
-          }
-        };
 
-        for (i=0; i < 100; i++) {
-          var element = document.getElementById(addZeros(numString[i]));
-          $(element).attr("enabled", "enabled")
-          $(element).parent().parent().addClass( "green");
-        };
-      });
+    $("div.container").on('click', "button#display_board", function(e){
+      var numString = $("div#num_pool").text();
+      numString = numString.replace(/\n/g, "");
+      numString = numString.match(/\d{1,2}/g)
+      console.log(numString)
+      for (i = 0; i < 100; i++) {
+        if (!numString.includes(i.toString() )) {
+          console.log(i.toString())
+          var taken = document.getElementById(addZeros(i).toString())
+          $(taken).parent().parent().addClass( "black");
+          $(taken).attr("disabled", "disabled")
+        }
+      };
+
+      for (i=0; i < 100; i++) {
+        var element = document.getElementById(addZeros(numString[i]));
+        $(element).attr("enabled", "enabled")
+        $(element).parent().parent().addClass( "green");
+      };
+    });
 
     function addZeros(n) {
-    // return (n < 10)? Number('0' + n) : i;
-    return (n < 10 ? '0' : '') + n;
+      return (n < 10 ? '0' : '') + n;
   }
 
 
-    $("div.container").on('click', "div.contest-card", function(e){
-      var id = $("div.contest-card").attr("contest_id");
-    });
+  $("div.container").on('click', "div.contest-card", function(e){
+    var id = $("div.contest-card").attr("contest_id");
+  });
   
 
-    $("div.container").on("click", "div.square", function(){
-      var contestId = $("form#form").attr("action").match(/\d+/);
-         // alert($(this).html());
-         console.log(this)
-         $(this).removeClass('green');
-         $(this).css("background-color", "red");
-         $("input[type=checkbox]", this).attr("checked", "checked");
-      })
-
-    $('form#form').on('submit', "div.square", function(){
-        $(this).addClass('black');
-
-    } )
-  
+  $("div.container").on("click", "div.square", function(){
+    var contestId = $("form#form").attr("action").match(/\d+/);
+    $(this).removeClass('green');
+    $(this).css("background-color", "brown");
+    $("input[type=checkbox]", this).attr("checked", "checked");
+  })
+});
 
 
-    //   $.ajax({
-    //     url: "/contests/" + id, 
-    //   }).done(function(result){
-    //     // readyBoard(result);
-    //   });
-    // });
-
-    // }
 
 
-    // function readyBoard(response) {
-    //   for (i=0; i < response.length; i++) {
-    //     var element = document.getElementById(response[i]);
-    //     console.log(element)
-    //     $(element).removeAttr("disabled");
-    //     $(element).addClass("enabled");
-    //   };
-    // };
+
+
+
+
 
 
 
@@ -82,10 +62,10 @@
 
   // $("div.container").on('click', "div.square", function(event){
   //  var contestId = $("form#form").attr("action").charAt(10);
-  //       	// console.log(JSON.stringify(numPool))
+  //        // console.log(JSON.stringify(numPool))
 
-  //       	alert($(this).html());
-  //       	// event.preventDefault();
+  //        alert($(this).html());
+  //        // event.preventDefault();
   //         var test = $(this).find("input[disabled='disabled']").attr("id")
   //         console.log("Test - " + this)
   //         // if (this.children().children().attr("disabled").val("disabled")) {};
@@ -95,7 +75,6 @@
   //         var cellRoute = "/contests/" + contestId + "/cells";
   //       });
 
-});
 
 
 
