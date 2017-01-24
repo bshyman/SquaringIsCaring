99cb45c82d87ed2d33140cbd97c28f9dd4cb0393
Benjamin Shyman <bshyman@gmail.com>
2017-01-24 10:04:13 -0600

    Made time zone UTC friendly

--- app/helpers/contests_helper.rb
+++ app/helpers/contests_helper.rb
@@ -14,7 +14,7 @@ module ContestsHelper
   def away_axis_populater(board, x)
     axis_val = board.away_axis[x]
     return "<div class='row #{x}'>
-		<div class='col s1 away_num'>" +  axis_val + "</div>"
+    <div class='col s1 away_num'>" +  axis_val + "</div>"
   end
 
   def row_checkbox_populater(row)
@@ -23,35 +23,46 @@ module ContestsHelper
     10.times do
       string +=
       "<div class='col s1 square'>
-			<label for='#{row}#{z}' value='#{row}#{z}'>
-			<input type='checkbox' id='#{row}#{z}' name='cell[position][]' value='#{row}#{z}' >
-			</label>
-			</div>"
+      <label for='#{row}#{z}' value='#{row}#{z}'>
+      <input type='checkbox' id='#{row}#{z}' name='cell[position][]' value='#{row}#{z}' >
+      </label>
+      </div>"
       z += 1
     end
     return string + "</div>"
   end
 
   def format_num(number)
-  	if number[0] == "0"
-  		number = number[1]
-  	end
-  	number
+    if number[0] == "0"
+      number = number[1]
+    end
+    number
   end
 
   def remove_selected_nums(board, array)
-  	array.each do |number|
-    	board.available_nums.delete(format_num(number))
+    array.each do |number|
+      board.available_nums.delete(format_num(number))
     end
     board.available_nums
   end
 
   def ended?(board)
-    @board.event_date <= DateTime.now || @board.available_nums.length == 0  
+    board.event_date <= DateTime.now || board.available_nums.length == 0
   end
 
   def pick_winning_cells(board)
-    g
+
+  end
+
+  def display_event_date_or_message(board)
+    if board.event_date <= DateTime.now.utc
+      return "Sorry. This Event Has Begun"
+    else
+      if board.available_nums.length == 0
+        return "Sorry. All Cells Are Taken"
+      end
+    end
+    board.event_date.utc
   end
 
 end
