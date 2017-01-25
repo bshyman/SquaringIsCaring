ec0cb3fd97f340bb99b16c6d48bd5797f3fc7737
Benjamin Shyman <bshyman@gmail.com>
2017-01-25 17:48:48 -0600

    iterate and assign 1 position to each cell object

--- app/controllers/cells_controller.rb
+++ app/controllers/cells_controller.rb
@@ -7,15 +7,21 @@ class CellsController < ApplicationController
 			@msg = "Error. Event has started or no squares available"
 			
 		else
-			@cell = Cell.new(cell_params)
-			@cell.user_id = current_user.id
-			@cell.save
-			new_board = remove_selected_nums(@board, @cell.position)
-			if @cell.persisted?
-				@board.update_attributes(:available_nums => new_board)
-			else
-				flash[:error] = "Error. Cell not saved"
-				redirect_to login_path
+			cell_params[:position].each do |cell|
+
+				@cell = Cell.new(cell_params)
+				@cell.position = [cell]
+				p @cell
+				@cell.user_id = current_user.id
+				@cell.save
+				new_board = remove_selected_nums(@board, @cell.position)
+				
+				if @cell.persisted?
+					@board.update_attributes(:available_nums => new_board)
+				else
+					flash[:error] = "Error. Cell not saved"
+					redirect_to login_path
+				end
 			end
 		end
 			redirect_to contest_path(@board)
