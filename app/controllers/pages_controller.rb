68377dbcace4245ab28478f30181bf2ac2500045
Benjamin Shyman <bshyman@gmail.com>
2017-02-16 08:17:06 -0600

    Fix typo in dashboard route

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
 
 	def landing
 		if current_user
-			redirect_to 'dashboard'
+			redirect_to "/users/#{current_user.id}/dashboard"
 		else
 			render 'landing', layout: false
 		end
